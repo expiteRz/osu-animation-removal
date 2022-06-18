e2d3fd2976e07c1329313203bd051cc7a1b438a1
expiteRz <51064411+expiteRz@users.noreply.github.com>
2022-06-19 01:15:23 +0900

    Add confirmation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fmt::Error;
 use std::fs;
+use std::process::exit;
 use clap::Parser;
+use question::{Answer, Question};
 use regex::Regex;
 
 #[derive(Parser)]
@@ -10,7 +12,7 @@ use regex::Regex;
 #[clap(about = "Removes animation frames to create lite version for your osu! skin", long_about = None)]
 struct Cli {
     /// Specify the path to animated elements
-    #[clap(short, long, value_parser)]
+    #[clap(value_parser)]
     path: String,
 
     /// Includes 'followpoint' element into removal candidate
@@ -22,8 +24,13 @@ const REGEX: &str = "(hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g|menu-back
 const REGEX_F: &str = "(followpoint|hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g|menu-back|play-skip|scorebar-colour)-.*(\\d+)";
 
 #[tokio::main]
-async fn main(){
+async fn main() {
     let args = Cli::parse();
+
+    if !attention_removal() {
+        println!("Abort!");
+        exit(0);
+    }
 
     remove_files(args.path, args.followpoint).await.expect("Failed to remove animated elements");
 
@@ -48,4 +55,19 @@ async fn remove_files(p: String, f: bool) -> Result<(), Error> {
     }
 
     Ok(())
+}
+
+fn attention_removal() -> bool {
+    loop {
+        let q = Question::new("Before proceeding, make sure non-animated element has been prepared.\nDo you want to continue?")
+            .yes_no()
+            .until_acceptable()
+            .ask();
+
+        match q.unwrap() {
+            Answer::YES => return true,
+            Answer::NO => return false,
+            _ => continue
+        }
+    }
 }
