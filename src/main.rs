a4cd0e5d690830b8cc8bb0a5818b8b0cc60ff1c4
expiteRz <51064411+expiteRz@users.noreply.github.com>
2022-06-20 22:12:59 +0900

    Ready for new features - Refactor followpoint option information - New parameters will use for new features later - Simplify alert function

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,47 @@ struct Cli {
     #[clap(value_parser)]
     path: String,
 
-    /// Includes 'followpoint' element into removal candidate
+    /// Trigger to remove animated 'followpoint' element.
+    /// If enabled, the tool will remove all 'followpoint' elements with suffix of numbers
     #[clap(short, long, action)]
     followpoint: bool,
+
+    /// Trigger to remove all animated 'hit' related elements.
+    /// If enabled, the tool will remove all 'hit' related elements with suffix of numbers
+    #[clap(short('H'), long, action)]
+    hits: bool,
+
+    /// Specify the frame number to use as non-animated 'followpoint' element
+    #[clap(long, value_parser, value_name("untyped int"), default_value_t = 0)]
+    pre_followpoint_number: u32,
+
+    /// Specify the frame number to use as non-animated 'hit' related elements
+    #[clap(long, value_parser, value_name("untyped int"), default_value_t = 0)]
+    pre_hits_number: u32,
+
+    /// Specify the frame number to use as non-animated 'menu-back' element
+    #[clap(long, value_parser, value_name("untyped int"), default_value_t = 0)]
+    pre_menu_back_number: u32,
+
+    /// Specify the frame number to use as non-animated 'play-skip' element
+    #[clap(long, value_parser, value_name("untyped int"), default_value_t = 0)]
+    pre_play_skip_number: u32,
+}
+
+struct Store {
+    path: String,
+    followpoint: bool,
+    hits: bool,
+    pre_followpoint_number: u32,
+    pre_hits_number: u32,
+    pre_menu_back_number: u32,
+    pre_play_skip_number: u32,
 }
 
 const REGEX: &str = "(hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g|menu-back|play-skip|scorebar-colour)-.*(\\d+)";
 const REGEX_F: &str = "(followpoint|hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g|menu-back|play-skip|scorebar-colour)-.*(\\d+)";
+const REGEX_H_O: &str = "(hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g)-0.*";
+const REGEX_FORMAT: &str = "(hit0|hit50|hit100|hit100k|hit300|hit300k|hit300g)-{}.*";
 
 #[tokio::main]
 async fn main() {
@@ -32,24 +66,41 @@ async fn main() {
         exit(0);
     }
 
-    remove_files(args.path, args.followpoint).await.expect("Failed to remove animated elements");
+    let s = Store {
+        path: args.path,
+        followpoint: args.followpoint,
+        hits: args.hits,
+        pre_followpoint_number: args.pre_followpoint_number,
+        pre_hits_number: args.pre_hits_number,
+        pre_menu_back_number: args.pre_menu_back_number,
+        pre_play_skip_number: args.pre_play_skip_number,
+    };
+
+    remove_files(s).await.expect("Failed to remove animated elements");
 
     println!("Completed!")
 }
 
-async fn remove_files(p: String, f: bool) -> Result<(), Error> {
+async fn remove_files(s: Store) -> Result<(), Error> {
     let r: Regex;
-    if !f {
+    if !s.followpoint {
         r = Regex::new(REGEX).unwrap();
     } else {
         r = Regex::new(REGEX_F).unwrap();
     }
 
-    let dir = fs::read_dir(p).expect("Failed to read specified directory");
+    let r_h = Regex::new(REGEX_H_O).unwrap();
+
+    let dir = fs::read_dir(s.path).expect("Failed to read specified directory");
 
     for d in dir {
         let f = d.expect("Failed to read file in specified directory");
         if r.is_match(f.file_name().to_str().unwrap()) {
+            if !s.hits {
+                if r_h.is_match(f.file_name().to_str().unwrap()) {
+                    continue;
+                }
+            }
             fs::remove_file(f.path().to_str().unwrap()).expect("Failed to remove file");
         }
     }
@@ -58,16 +109,12 @@ async fn remove_files(p: String, f: bool) -> Result<(), Error> {
 }
 
 fn attention_removal() -> bool {
-    loop {
-        let q = Question::new("Before proceeding, make sure non-animated element has been prepared.\nDo you want to continue?")
-            .yes_no()
-            .until_acceptable()
-            .ask();
+    let q = Question::new("Before proceeding, make sure non-animated element has been prepared.\nDo you want to continue?")
+        .confirm();
 
-        match q.unwrap() {
-            Answer::YES => return true,
-            Answer::NO => return false,
-            _ => continue
-        }
+    match q {
+        Answer::YES => true,
+        Answer::NO => false,
+        _ => false
     }
 }
